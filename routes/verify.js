5cf2aeb8016305c4f7f327c877116339b6097177
Raz Consta <66283215+RazConsta@users.noreply.github.com>
2022-11-06 18:47:15 -0800

    a

--- routes/verify.js
+++ routes/verify.js
@@ -38,7 +38,7 @@ router.get('/', (request, response) => {
             let secondQuery = 'UPDATE members SET verification = 1 WHERE email = $1'
             pool.query(secondQuery, values) 
                 .then(result => {
-                    response.redirect('https://cultivate-app-web-service.herokuapp.com/verify/success')
+                    response.redirect('https://cultivate-app.nicepage.io/Home.html')
                 })
                 .catch(result => {
                     response.status(400).send({
