9ad6df7f84d035ee604ffb0e2e55256ec1ddecef
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-12-08 00:04:32 -0800

    Update 24HourWeather.js

--- routes/weather/24HourWeather.js
+++ routes/weather/24HourWeather.js
@@ -4,8 +4,8 @@ const express = require('express')
 const router = express.Router()
 require('dotenv').config()
 
-async function getWeather() {
-    const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?lat=47.11&lon=-122.11&cnt=8&units=imperial&appid=' + process.env.WEATHER_API_KEY
+async function getWeather(long, lat) {
+    const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&cnt=8&units=imperial&appid=' + process.env.WEATHER_API_KEY
     const request = await fetch(WEATHER_API_URL);
     const data = request.json();
     return data;
@@ -16,22 +16,30 @@ async function getWeather() {
  * @apiName Get24HourWeather
  * @apiGroup Weather
  */ 
-router.get('/', async (request, response) => {
+router.post('/', async (request, response) => {
 
-    getWeather()
+    getWeather(request.body.longitude, request.body.latitude)
         .then((weather) => {
+            //get the time zone from the weather data
+            let secondsFromUTC = weather.city.timezone;
+            let hoursFromUTC = secondsFromUTC / 3600;
+            
+            console.log(hoursFromUTC);
             console.log(weather);
             let weatherArray = {};
             let i = 0;
             weather.list.forEach((day) => {
+                console.log(day);
                 //store each weather temperature, conditions and time in an array
                 let temp = Math.round(day.main.temp);
+                console.log(temp);
                 let conditions = day.weather[0].main;
                 //get only the time from the date
-                let time = day.dt_txt.split(' ')[1];
+                let time = day.dt_txt.split(' ')[1];                
                 
                 //Turn the time into an integer and add am or pm
                 let timeInt = parseInt(time);
+                time -= hoursFromUTC;
                 let ampm = 'am';
                 if (timeInt == 0) {
                     timeInt = 12;
